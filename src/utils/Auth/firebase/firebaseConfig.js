91f23afea1f3e7be590cf9e60bc8099795b49d72
duyvu-fsdev <duyvu.fullstackdev@gmail.com>
2024-12-07 14:46:39 +0700

    #19.1. test firebase

--- src/utils/Auth/firebase/firebaseConfig.js
+++ src/utils/Auth/firebase/firebaseConfig.js
@@ -12,6 +12,8 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+(() => console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID))();
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
